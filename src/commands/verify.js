fe5445373cb4967638061a5734aacde0e992689b
mage <tifenn.fl@gmail.com>
2021-12-28 23:13:10 +0100

    verify

--- src/commands/verify.js
+++ src/commands/verify.js
@@ -22,9 +22,9 @@ export default {
 
 		const player_list = resolvePlayerList(data);
 
-		const content = `Tournoi généré: ${
+		const content = `Pools randoms générées: ${
 			data.tournament ? "Oui" : "Non"
-		}\nInscriptions ouvertes: ${
+		}\nNom du tournoi: ${data.tournament_name}\nInscriptions ouvertes: ${
 			data.open ? "Oui" : "Non"
 		}\nCapacité du tournoi: ${player_list.max_amount}\nJoueurs inscrits: ${
 			player_list.current_amount
