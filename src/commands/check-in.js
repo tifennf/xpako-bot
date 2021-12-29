9ec9fe4861f05d99644b541e811a173c3081eaf1
mage <tifenn.fl@gmail.com>
2021-12-29 20:11:19 +0100

    check-in

--- src/commands/check-in.js
+++ src/commands/check-in.js
@@ -19,31 +19,35 @@ export default {
 
 		const which_message = options[0].value;
 
-		if (!(which_message === 1 || which_message === 2)) {
-			throw "Aucun message sélectionné";
-		}
-
-		const message_list = [
-			"Hello,\n\nJe t'envoie ce message pour te rappeler **ton inscription à notre tournoi hebdomadaire !**\n\nIl aura lieu demain soir sur notre serveur discord : https://discord.gg/SkMSdyTFjd\n\n**Est-ce que tu confirmes ta présence ?**\n\nMerci d'avance de ta réponse, et good luck !",
-			"Hello,\n\nNotre tournoi débute dans **15 minutes !**\n\nTiens toi prêt et rejoint notre Discord : https://discord.gg/SkMSdyTFjd\n\nSi tu es dans **l'incapacité de partager** ce bon moment avec nous, **utilise la réaction 🟥 afin de libérer ta place !**\n\nA tout de suite !",
-		];
-
-		const id_list = data.player_list.list.map((player) => player.discord_id);
-
-		const guildMembers = await interaction.guild.members.fetch();
-
-		guildMembers.each((guildMember) => {
-			const { user } = guildMember;
-
-			const { id } = user;
-
-			if (id_list.includes(id)) {
-				const content = message_list[which_message];
-
-				user.send(content);
+		try {
+			if (which_message < 1 || which_message > 2) {
+				throw "Aucun message sélectionné";
 			}
-		});
 
-		interaction.reply("Check-in fait");
+			const message_list = [
+				"Hello,\n\nJe t'envoie ce message pour te rappeler **ton inscription à notre tournoi hebdomadaire !**\n\nIl aura lieu demain soir sur notre serveur discord : https://discord.gg/SkMSdyTFjd\n\n**Est-ce que tu confirmes ta présence ?**\n\nMerci d'avance de ta réponse, et good luck !",
+				"Hello,\n\nNotre tournoi débute dans **15 minutes !**\n\nTiens toi prêt et rejoint notre Discord : https://discord.gg/SkMSdyTFjd\n\nSi tu es dans **l'incapacité de partager** ce bon moment avec nous, **utilise la réaction 🟥 afin de libérer ta place !**\n\nA tout de suite !",
+			];
+
+			const id_list = data.player_list.list.map((player) => player.discord_id);
+
+			const guildMembers = await interaction.guild.members.fetch();
+
+			guildMembers.each((guildMember) => {
+				const { user } = guildMember;
+
+				const { id } = user;
+
+				if (id_list.includes(id)) {
+					const content = message_list[which_message];
+
+					user.send(content);
+				}
+			});
+
+			interaction.reply("Check-in fait");
+		} catch (error) {
+			interaction.reply(error);
+		}
 	},
 };
