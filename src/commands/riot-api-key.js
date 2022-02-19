97c15efba81717312a9e21f99e429fd11f575056
root <tifenn.fl@gmail.com>
2022-02-19 14:46:45 +0000

    fix

--- src/commands/riot-api-key.js
+++ src/commands/riot-api-key.js
@@ -4,8 +4,8 @@ import toml from "toml";
 
 export default {
 	data: new SlashCommandBuilder()
-		.setName("init")
-		.setDescription("Démarre une session de tournoi")
+		.setName("riot-api-key")
+		.setDescription("Change la clef riot api")
 		.setDefaultPermission(false)
 		.addStringOption((option) =>
 			option.setName("key").setDescription("Clef api riot").setRequired(true)
@@ -16,7 +16,7 @@ export default {
 		const key = options[0].value;
 
 		try {
-			const file = await fs.readFile("~/services/tournament-core/config.toml");
+			const file = await fs.readFile("../../../services/tournament-core/config.toml");
 
 			const config = toml.parse(file);
 
